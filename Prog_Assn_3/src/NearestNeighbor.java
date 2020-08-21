bba2ecfa2bdc36adf1ab0f5dfa1c3f88a2c1cf66
KiMiBr330i <68255276+KiMiBr330i@users.noreply.github.com>
2020-08-21 07:04:43 -0600

    commit 08-21-20 0704hrs.

--- Prog_Assn_3/src/NearestNeighbor.java
+++ Prog_Assn_3/src/NearestNeighbor.java
@@ -5,44 +5,61 @@ import java.util.Arrays;
 public class NearestNeighbor {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		
-	
+
 		Scanner testing = new Scanner(System.in);
 		Scanner training = new Scanner(System.in);
 
 //System.out.print("Enter the name for the training file: ");
-String trainingFile = "/Users/kipbrower/Downloads/iris-training-data.csv";
+		String trainingFile = "/Users/kipbrower/Desktop/iris-training-data.csv";
 //System.out.print("Enter the name for the testing file: ");
-String testingFile = "/Users/kipbrower/Downloads/iris-testing-data.csv";
-File testingData = new File("/Users/kipbrower/Downloads/iris-testing-data.csv");
-File trainingData = new File("/Users/kipbrower/Downloads/iris-training-data.csv");
-Scanner trainScan = new Scanner(trainingData);
-Scanner testScan = new Scanner(testingData);
-
+		String testingFile = "/Users/kipbrower/Desktop/iris-testing-data.csv";
+		File testingData = new File("/Users/kipbrower/Desktop/iris-testing-data.csv");
+		File trainingData = new File("/Users/kipbrower/Desktop/iris-training-data.csv");
+		Scanner trainScan = new Scanner(trainingData);
+		Scanner testScan = new Scanner(testingData);
 
 // making arrays for training data ----------------------------------------------------------------
-while (trainScan.hasNext()) {
-	String aData = trainScan.nextLine();
-	String[] aData1 = aData.split(",");
+		while (trainScan.hasNext()) {
+			String aData = trainScan.nextLine();
+			
+			String[] aData1 = aData.split(",") ;
+			
+			System.out.println(aData1[4]);
+			
+			String[] a_class = aData1[4].split(",");
+			System.out.println(a_class[]);
+			
+			
+				}
+				
+				
+			}
+		}
+// making arrays for testing data -------------------------------------------------------------------
+			
+			 /* while (testScan.hasNext()) { 
+				  String bData = testScan.nextLine(); 
+			  String[] bData1 = bData.split(","); 
+			  final double bS_LENGTH = Double.parseDouble(bData1[0]); 
+			  final double bS_WIDTH = Double.parseDouble(bData1[1]); 
+			  final double bP_LENGTH = Double.parseDouble(bData1[2]); 
+			  final double bP_WIDTH = Double.parseDouble(bData1[3]);
+			  String[] test_class = {bData1[4]};
+			  double[][] test_dat = { { Double.parseDouble(bData1[0]) }, { Double.parseDouble(bData1[1]) },
+						{ Double.parseDouble(bData1[2]) }, { Double.parseDouble(bData1[3]) } };
+				
+			  for (int row = 0; row < test_dat.length; row++) {
+					for (int col = 0; col < test_dat[row].length; col++)
+						System.out.println(test_dat[row][col]);
+					System.out.println();
+			  }*/
+			/*
+			 * double sepal_l = Math.pow((aS_LENGTH - bS_LENGTH), 2); double sepal_w =
+			 * Math.pow((aS_WIDTH - bS_WIDTH), 2); double petal_l = Math.pow((aP_LENGTH -
+			 * bP_LENGTH), 2); double petal_w = Math.pow((aP_WIDTH - bP_WIDTH), 2); double
+			 * dist = Math.sqrt((sepal_l) +(sepal_w) + (petal_l) + (petal_w));
+			 * System.out.println(dist);
+			 */
+		//}
+	//}
 
-//Double.parseDouble(aData1[0]);
-double S_LENGTH = Double.parseDouble(aData1[0]);
-System.out.println(S_LENGTH);
-//String S_WIDTH = aData1[1];
-//Double.parseDouble(aData1[1]);
-double S_WIDTH = Double.parseDouble(aData1[1]);
-System.out.println(S_WIDTH);
-//String[] P_LENGTH = aData1[2];
-//String[] P_WIDTH = aData1[3];
-//String[] clas = aData1[4];
-double[][] sepal = {Double.parseDouble(aData1[0]),
-					Double.parseDouble(aData1[1])};
-}
-
-
-
-
-//String eData = testScan.nextLine();{
-}
-
-}
