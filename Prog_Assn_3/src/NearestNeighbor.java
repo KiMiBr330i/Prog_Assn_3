7afd318cfc0211b34fd2e2cf1057f7f25f65ea08
KiMiBr330i <68255276+KiMiBr330i@users.noreply.github.com>
2020-08-22 07:33:14 -0600

    commit 08-22-20 0733hrs.

--- Prog_Assn_3/src/NearestNeighbor.java
+++ Prog_Assn_3/src/NearestNeighbor.java
@@ -3,7 +3,7 @@ import java.io.*;
 import java.util.Arrays;
 
 public class NearestNeighbor {
-
+	static double s_len, s_wid, p_len, p_wid;
 	public static void main(String[] args) throws FileNotFoundException {
 
 		Scanner testing = new Scanner(System.in);
@@ -17,27 +17,29 @@ public class NearestNeighbor {
 		File trainingData = new File("/Users/kipbrower/Desktop/iris-training-data.csv");
 		Scanner trainScan = new Scanner(trainingData);
 		Scanner testScan = new Scanner(testingData);
-
+		
+		//String [] aData1;
 // making arrays for training data ----------------------------------------------------------------
 		while (trainScan.hasNext()) {
 			String aData = trainScan.nextLine();
 			
-			String[] aData1 = aData.split(",") ;
-			
-			System.out.println(aData1[4]);
-			
-			String[] a_class = aData1[4].split(",");
-			System.out.println(a_class[]);
+			String[] aData1 = aData.split(",");
+			double [][] train_dat = new double[74][3];
+		 for (int i = 0; i < aData1.length; i++)
+			 for (int j = 0; j < aData1[0].length(); j++)
 			
 			
-				}
-				
-				
+			
+			
+						
+//double[][] train_dat = {{s_len, s_wid, p_len, p_wid}};
+		System.out.print(train_dat);
+		 System.out.println();
 			}
-		}
+		
 // making arrays for testing data -------------------------------------------------------------------
 			
-			 /* while (testScan.hasNext()) { 
+			/* while (testScan.hasNext()) { 
 				  String bData = testScan.nextLine(); 
 			  String[] bData1 = bData.split(","); 
 			  final double bS_LENGTH = Double.parseDouble(bData1[0]); 
@@ -52,14 +54,14 @@ public class NearestNeighbor {
 					for (int col = 0; col < test_dat[row].length; col++)
 						System.out.println(test_dat[row][col]);
 					System.out.println();
-			  }*/
-			/*
+			  }
+			
 			 * double sepal_l = Math.pow((aS_LENGTH - bS_LENGTH), 2); double sepal_w =
 			 * Math.pow((aS_WIDTH - bS_WIDTH), 2); double petal_l = Math.pow((aP_LENGTH -
 			 * bP_LENGTH), 2); double petal_w = Math.pow((aP_WIDTH - bP_WIDTH), 2); double
 			 * dist = Math.sqrt((sepal_l) +(sepal_w) + (petal_l) + (petal_w));
 			 * System.out.println(dist);
 			 */
-		//}
-	//}
-
+		
+	}
+}
