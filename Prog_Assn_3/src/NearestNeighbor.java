1707b980ef4ef67491c3c311f1f939b322efda93
KiMiBr330i <68255276+KiMiBr330i@users.noreply.github.com>
2020-08-22 22:59:41 -0600

    commit 08-22-2020 2259hrs.

--- Prog_Assn_3/src/NearestNeighbor.java
+++ Prog_Assn_3/src/NearestNeighbor.java
@@ -4,11 +4,12 @@ import java.util.Arrays;
 
 public class NearestNeighbor {
 	static double s_len, s_wid, p_len, p_wid;
+
 	public static void main(String[] args) throws FileNotFoundException {
 
 		Scanner testing = new Scanner(System.in);
 		Scanner training = new Scanner(System.in);
-
+	
 //System.out.print("Enter the name for the training file: ");
 		String trainingFile = "/Users/kipbrower/Desktop/iris-training-data.csv";
 //System.out.print("Enter the name for the testing file: ");
@@ -17,65 +18,68 @@ public class NearestNeighbor {
 		File trainingData = new File("/Users/kipbrower/Desktop/iris-training-data.csv");
 		Scanner trainScan = new Scanner(trainingData);
 		Scanner testScan = new Scanner(testingData);
-		
-		//String [] aData1;
+
+		// String [] aData1;
 // making arrays for training data ----------------------------------------------------------------
-		while (trainScan.hasNext()) {
+		if (trainScan.hasNext()) {
 			String aData = trainScan.nextLine();
-			
-			String[] aData1 = aData.split(",");
-			
-			double[] s_l = new double[75];	
-			String[] aData2 = aData1[0].split(",");
-			for (int i = 0; i < aData2.length; i++)
-				s_l[i] aData2 = aData1[0].split(",");
-			
-				//System.out.println();
-			//String aData2_a = Arrays.toString(aData2);
-			//double [] aData2_1 = Double.parseDouble(aData2_a);
-			
-			//final double s_len = Double.parseDouble(aData1[0]);
-			//final double s_wid = Double.parseDouble(aData1[1]);
-			//final double p_len = Double.parseDouble(aData1[2]);
-			//final double p_wid = Double.parseDouble(aData1[3]);
-			
-			//double[] s_l = new double[75];
-			//for(int i = 0; i < s_l.length; i++)
-			//	s_l[i] = Double.parseDouble(aData2_1);
-			//	System.out.println(aData1[0]);
-			
-			
-			
+
+			//for (int i=0; i < aData.length()-1; i++)
+			System.out.println(aData.length());
 				
-		//double[][] train_dat = {{s_len, s_wid, p_len, p_wid}};
-		//System.out.println(s_len);		
-			}
 		
-// making arrays for testing data -------------------------------------------------------------------
-			
-			/* while (testScan.hasNext()) { 
-				  String bData = testScan.nextLine(); 
-			  String[] bData1 = bData.split(","); 
-			  final double bS_LENGTH = Double.parseDouble(bData1[0]); 
-			  final double bS_WIDTH = Double.parseDouble(bData1[1]); 
-			  final double bP_LENGTH = Double.parseDouble(bData1[2]); 
-			  final double bP_WIDTH = Double.parseDouble(bData1[3]);
-			  String[] test_class = {bData1[4]};
-			  double[][] test_dat = { { Double.parseDouble(bData1[0]) }, { Double.parseDouble(bData1[1]) },
-						{ Double.parseDouble(bData1[2]) }, { Double.parseDouble(bData1[3]) } };
 				
-			  for (int row = 0; row < test_dat.length; row++) {
-					for (int col = 0; col < test_dat[row].length; col++)
-						System.out.println(test_dat[row][col]);
-					System.out.println();
-			  }
 			
-			 * double sepal_l = Math.pow((aS_LENGTH - bS_LENGTH), 2); double sepal_w =
-			 * Math.pow((aS_WIDTH - bS_WIDTH), 2); double petal_l = Math.pow((aP_LENGTH -
-			 * bP_LENGTH), 2); double petal_w = Math.pow((aP_WIDTH - bP_WIDTH), 2); double
-			 * dist = Math.sqrt((sepal_l) +(sepal_w) + (petal_l) + (petal_w));
-			 * System.out.println(dist);
-			 */
+			
+			//String[] aData1 = aData.split(",");
+
 		
+		// String[] aData2 = aData1[0].split(",");
+		//double[][] s_l = new double[4][75];
+		//for (int i = 0; i < aData1.length - 1; i++)
+		//	for (int j = 0; j < aData1[0].length(); j++)
+			
+		
+		//System.out.println();
+		//String aData_1 = Arrays.deepToString(aData);
+		//double [] aData2_1 = Double.parseDouble(aData2_a);
+
+		// final double s_len = Double.parseDouble(aData1[0]);
+		// final double s_wid = Double.parseDouble(aData1[1]);
+		// final double p_len = Double.parseDouble(aData1[2]);
+		// final double p_wid = Double.parseDouble(aData1[3]);
+
+		// double[] s_l = new double[75];
+		// for(int i = 0; i < s_l.length; i++)
+		// s_l[i] = Double.parseDouble(aData2_1);
+		// System.out.println(aData1[0]);
+
+		// double[][] train_dat = {{s_len, s_wid, p_len, p_wid}};
+		// System.out.println(s_len);
 	}
-}
+
+// making arrays for testing data -------------------------------------------------------------------
+
+	/*
+	 * while (testScan.hasNext()) { String bData = testScan.nextLine(); String[]
+	 * bData1 = bData.split(","); final double bS_LENGTH =
+	 * Double.parseDouble(bData1[0]); final double bS_WIDTH =
+	 * Double.parseDouble(bData1[1]); final double bP_LENGTH =
+	 * Double.parseDouble(bData1[2]); final double bP_WIDTH =
+	 * Double.parseDouble(bData1[3]); String[] test_class = {bData1[4]}; double[][]
+	 * test_dat = { { Double.parseDouble(bData1[0]) }, {
+	 * Double.parseDouble(bData1[1]) }, { Double.parseDouble(bData1[2]) }, {
+	 * Double.parseDouble(bData1[3]) } };
+	 * 
+	 * for (int row = 0; row < test_dat.length; row++) { for (int col = 0; col <
+	 * test_dat[row].length; col++) System.out.println(test_dat[row][col]);
+	 * System.out.println(); }
+	 * 
+	 * double sepal_l = Math.pow((aS_LENGTH - bS_LENGTH), 2); double sepal_w =
+	 * Math.pow((aS_WIDTH - bS_WIDTH), 2); double petal_l = Math.pow((aP_LENGTH -
+	 * bP_LENGTH), 2); double petal_w = Math.pow((aP_WIDTH - bP_WIDTH), 2); double
+	 * dist = Math.sqrt((sepal_l) +(sepal_w) + (petal_l) + (petal_w));
+	 * System.out.println(dist);
+	 */
+
+}}
